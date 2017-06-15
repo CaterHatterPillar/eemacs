8ec33e321ef3e494a3ed562497f0e1de72baf29b
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-06-15 16:55:41 -0700

    I've learned I save buffers to files late

--- init.el
+++ init.el
@@ -57,9 +57,13 @@
           '(lambda ()
              (define-key c-mode-map "\C-m" 'newline-and-indent)))
              
+(defun prompt-error-unless-visiting-buffer ()
+  (unless buffer-file-name (error "Buffer not visiting a file")))
+
 (defun copy-file-name ()
   "Copy the current buffer file name."
   (interactive)
+  (prompt-error-unless-visiting-buffer)
   (kill-new (file-name-nondirectory
 	   (buffer-file-name
 	    (window-buffer
@@ -68,6 +72,7 @@
 (defun copy-file-directory ()
   "Copy the current buffer directory."
   (interactive)
+  (prompt-error-unless-visiting-buffer)
   (kill-new (file-name-directory
 	   (buffer-file-name
 	    (window-buffer
@@ -76,6 +81,7 @@
 (defun copy-file-path ()
   "Copy the current buffer full path."
   (interactive)
+  (prompt-error-unless-visiting-buffer)
   (kill-new (buffer-file-name (window-buffer (minibuffer-selected-window)))))
 
 ;; Candidates
