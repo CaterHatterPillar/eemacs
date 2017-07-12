8cd668090e8377a89c09291945944c92457ff46e
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-07-12 08:38:11 -0700

    Get the appropriate indentation width for C in case a style is used

--- templates.el
+++ templates.el
@@ -1,3 +1,15 @@
+(defun c-basic-offset-from-style (style)
+  (cdr (assoc 'c-basic-offset (cdr (assoc style c-style-alist)))))
+
+(defun c-style-from-default-style ()
+  (let ((c_mode (assoc 'c-mode c-default-style)))
+    (cdr (if (eq c_mode nil) (assoc 'other c-default-style) c_mode))))
+
+(defun indentation-width-c ()
+  (if (eq c-basic-offset 'set-from-style)
+      (c-basic-offset-from-style (c-style-from-default-style))
+    c-basic-offset))
+
 (defun instantiate-template-c (name directory)
   "Create a minimal C program and Makefile"
   (interactive "sName: \nDDirectory: ")
@@ -9,11 +21,9 @@
     (let ((main_c (concat (file-name-as-directory project)
                           (format "%s.c" name))))
       (with-temp-buffer
-        (insert "int main(int argc, char* argv[]) {\n")
-        (insert (make-string (if (equal c-basic-offset 'set-from-style)
-                                 4 c-default-style)
-                             ?\ ))
-        (insert "return 0;\n}\n")
+        (insert (concat "int main(int argc, char* argv[]) {\n"
+                        (make-string (indentation-width-c) ?\ )
+                        "return 0;\n}\n"))
         (write-file main_c))
       (find-file main_c))
 
