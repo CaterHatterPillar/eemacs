ab7ac32fdcf765f1a5096bf07e469d34d90827cd
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-07-12 11:55:10 -0700

    Add a corresponding function for C++

--- templates.el
+++ templates.el
@@ -10,29 +10,44 @@
       (c-basic-offset-from-style (c-style-from-default-style))
     c-basic-offset))
 
-(defun instantiate-template-c (name directory)
-  "Create a minimal C program and Makefile"
-  (interactive "sName: \nDDirectory: ")
-
+(defun instantiate-template-c-like (name directory filename body)
   (let ((project (concat directory name)))
     (unless (file-exists-p project)
       (make-directory project))
 
-    (let ((main_c (concat (file-name-as-directory project)
-                          (format "%s.c" name)))
+    (let ((source_file (concat (file-name-as-directory project) filename))
           (indentation_width (make-string (indentation-width-c) ?\ )))
       (with-temp-buffer
-        (insert (concat "#include <stdio.h>\n\n"
-                        "int main(int argc, char* argv[]) {\n"
-                        indentation_width
-                        (format "printf(\"%s\\n\");\n" name)
-                        indentation_width
-                        "return 0;\n}\n"))
-        (write-file main_c))
-      (find-file main_c))
+        (insert (replace-regexp-in-string "\t" indentation_width body))
+        (write-file source_file))
+      (find-file source_file))
 
     (with-temp-buffer
       (insert (format "%s:\n" name))
-      (write-file (concat (file-name-as-directory project) "Makefile")))))
+      (write-file (concat (file-name-as-directory project) "Makefile"))))
+)
+
+(defun instantiate-template-c (name directory)
+  "Create a minimal C program and Makefile"
+  (interactive "sName: \nDDirectory: ")
+  (instantiate-template-c-like name directory (format "%s.c" name)
+                               (concat "#include <stdio.h>\n\n"
+                                       "int main(int argc, char* argv[]) {\n"
+                                       (format "\tprintf(\"%s\\n\");\n" name)
+                                       "\treturn 0;\n"
+                                       "}\n")))
+
+(defun instantiate-template-cc (name directory)
+  "Create a minimal C++ program and Makefile"
+  (interactive "sName: \nDDirectory: ")
+  (instantiate-template-c-like
+   name
+   directory
+   (format "%s.cc" name)
+   (concat "#include <iostream>\n\n"
+           "int main(int argc, char* argv[]) {\n"
+           (format "\tstd::cout << \"%s\\n\";\n" name)
+           "\treturn 0;\n"
+           "}\n")))
 
 (provide 'templates)
