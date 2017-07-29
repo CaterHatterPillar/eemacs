70752af788e71cadce46db0c7522c598582f09a0
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-07-29 10:53:32 -0700

    Group miscellaneous functions in a new file.

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 (require 'appearance)
 (require 'linenum)
 (require 'local nil t)
+(require 'miscfun)
 
 (require 'midnight)
 
@@ -76,34 +77,7 @@
 ;; Auto-indent after newline:
 (add-hook 'c-mode-hook
           '(lambda ()
-             (define-key c-mode-map "\C-m" 'newline-and-indent)))
-             
-(defun prompt-error-unless-visiting-buffer ()
-  (unless buffer-file-name (error "Buffer not visiting a file")))
-
-(defun copy-file-name ()
-  "Copy the current buffer file name."
-  (interactive)
-  (prompt-error-unless-visiting-buffer)
-  (kill-new (file-name-nondirectory
-	   (buffer-file-name
-	    (window-buffer
-	     (minibuffer-selected-window))))))
-
-(defun copy-file-directory ()
-  "Copy the current buffer directory."
-  (interactive)
-  (prompt-error-unless-visiting-buffer)
-  (kill-new (file-name-directory
-	   (buffer-file-name
-	    (window-buffer
-	     (minibuffer-selected-window))))))
-
-(defun copy-file-path ()
-  "Copy the current buffer full path."
-  (interactive)
-  (prompt-error-unless-visiting-buffer)
-  (kill-new (buffer-file-name (window-buffer (minibuffer-selected-window)))))
+             (define-key c-mode-map "\C-m" 'newline-and-indent)))             
 
 ;; Candidates
 ;; ----------
---
+++ miscfun.el
@@ -0,0 +1,28 @@
+(defun prompt-error-unless-visiting-buffer ()
+  (unless buffer-file-name (error "Buffer not visiting a file")))
+
+(defun copy-file-name ()
+  "Copy the current buffer file name."
+  (interactive)
+  (prompt-error-unless-visiting-buffer)
+  (kill-new (file-name-nondirectory
+	   (buffer-file-name
+	    (window-buffer
+	     (minibuffer-selected-window))))))
+
+(defun copy-file-directory ()
+  "Copy the current buffer directory."
+  (interactive)
+  (prompt-error-unless-visiting-buffer)
+  (kill-new (file-name-directory
+	   (buffer-file-name
+	    (window-buffer
+	     (minibuffer-selected-window))))))
+
+(defun copy-file-path ()
+  "Copy the current buffer full path."
+  (interactive)
+  (prompt-error-unless-visiting-buffer)
+  (kill-new (buffer-file-name (window-buffer (minibuffer-selected-window)))))
+
+(provide 'miscfun)
