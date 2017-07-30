75dfc2d6f6f1a82d69c77547a8f781d7f6384c75
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-07-30 16:13:46 -0700

    Another attempt at grouping things together

--- init.el
+++ init.el
@@ -5,64 +5,51 @@
 (require 'linenum)
 (require 'local nil t)
 (require 'miscfun)
+(require 'programming)
 (require 'template)
 
 (require 'midnight)
 
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-(setq uniquify-after-kill-buffer-p t)
-
-(require 'whitespace)
-(setq whitespace-style '(empty face lines-tail tabs trailing))
-(global-whitespace-mode t)
+;; State
 
 (setq savehist-file "~/.emacs.d/history")
 (setq savehist-additional-variables
       '(compile-history kill-ring regexp-search-ring search-ring))
 (savehist-mode 1)
 
+(setq backup-by-copying t
+      backup-directory-alist
+      '(("." . "~/.emacs.d/backups"))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
+;; Buffers
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-after-kill-buffer-p t)
+
 (global-auto-revert-mode 1)
-(show-paren-mode 1)
-(winner-mode 1)
-
-(setq semantic-default-submodes
-      '(global-semantic-idle-scheduler-mode
-        global-semanticdb-minor-mode
-        global-semantic-decoration-mode
-        global-semantic-highlight-func-mode
-        global-semantic-stickyfunc-mode
-        global-semantic-idle-summary-mode
-        global-semantic-idle-breadcrumbs-mode
-        global-semantic-mru-bookmark-mode))
-(add-hook 'prog-mode-hook 'semantic-mode)
-
-(windmove-default-keybindings)
 
 (setq confirm-nonexistent-file-or-buffer nil)
-(setq font-lock-global-modes '(not test-log-mode))
-(setq ido-default-buffer-method 'selected-window)
-(setq ring-bell-function 'ignore)
-(setq-default indent-tabs-mode nil)
 
 (setq scroll-step            1
       scroll-conservatively  10000)
 
-(setq
-   backup-by-copying t
-   backup-directory-alist
-    '(("." . "~/.emacs.d/backups"))
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)
+;; Windows
+
+(winner-mode 1)
+(windmove-default-keybindings)
+
+;; Misc
+
+(setq font-lock-global-modes '(not test-log-mode))
+(setq ido-default-buffer-method 'selected-window)
+(setq ring-bell-function 'ignore)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Auto-indent after newline:
-(add-hook 'c-mode-hook
-          '(lambda ()
-             (define-key c-mode-map "\C-m" 'newline-and-indent)))             
 
 ;; Candidates
 ;; ----------
