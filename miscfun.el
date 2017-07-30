8962571f502b8678ae96a6fd58c5131ed8e93eff
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-07-30 09:50:35 -0700

    Although process-attributes isn't supported on OS X, a function like this should prove useful on other platforms. Add a reasonable prototype.

--- miscfun.el
+++ miscfun.el
@@ -1,3 +1,25 @@
+(require 'cl)
+
+(defun pred (pid name)
+  (let ((user (user-uid))
+        (attributes (process-attributes pid)))
+    (and (eq (assoc 'euid attributes) user)
+         (eq (assoc 'comm attributes) name))))
+
+(defun matching-pids (name)
+  (let ((pred (lambda (pid) (funcall #'pred pid name)))
+        (pids (list-system-processes)))
+    (remove-if-not pred pids)))
+
+(defun debug-program (name)
+  "Attempt to attach gdb to a process matching name"
+  (interactive "sName: ")
+  (let ((pids (matching-pids name)))
+    (let ((num_pids (length pids)))
+      (cond ((eq num_pids 0) (error "No matching process found"))
+            ((eq num_pids 1) (gdb (format "gdb --pid %d" (car pids))))
+            (t (error "Multiple matches found"))))))
+
 (defun current-file-name ()
   (unless buffer-file-name (error "Buffer not visiting a file"))
   (buffer-file-name (window-buffer (minibuffer-selected-window))))
