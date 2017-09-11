4e8ea071ef93c7b3a31a1dfdee43269db89b458a
Eric Nilsson <CaterHatterPillar@gmail.com>
2017-09-11 12:27:24 -0700

    Define an alias for rename-uniquely

--- aliases.el
+++ aliases.el
@@ -1,6 +1,7 @@
 (defalias 'dl 'delete-matching-lines)
 (defalias 'kl 'keep-lines)
 (defalias 'rb 'rename-buffer)
+(defalias 'ru 'rename-uniquely)
 (defalias 'sl 'sort-lines)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
