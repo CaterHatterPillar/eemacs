ba1c50f3e4c64737fefb88b1b288a84a3adb6b2c
Eric Nilsson <eric.nilsson@intel.com>
2018-02-28 17:01:52 +0100

    Lines are expensive nowadays

--- elisp/programming.el
+++ elisp/programming.el
@@ -46,13 +46,12 @@
 
 (require 'diff)
 (defun delete-trailing-whitespace-at-modified-lines ()
-  (when buffer-file-name
-    (when (derived-mode-p 'prog-mode)
-      (save-excursion
-        (with-current-buffer
-            (diff-no-select buffer-file-name (current-buffer) nil 'noasync)
-          (diff-delete-trailing-whitespace)
-          (kill-buffer))))))
+  (when (and buffer-file-name (derived-mode-p 'prog-mode))
+    (save-excursion
+      (with-current-buffer
+          (diff-no-select buffer-file-name (current-buffer) nil 'noasync)
+        (diff-delete-trailing-whitespace)
+        (kill-buffer)))))
 
 (when (eq system-type 'gnu/linux)
   (add-hook 'before-save-hook 'delete-trailing-whitespace-at-modified-lines))
