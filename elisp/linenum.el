b6a5482ff543542964e5065dc8a0dd9264b861c3
Eric Nilsson <eric.nilsson@intel.com>
2018-03-09 09:29:01 +0100

    linum-update-current appears not to be a function in Emacs 23

--- elisp/linenum.el
+++ elisp/linenum.el
@@ -1,10 +1,11 @@
 (add-hook 'prog-mode-hook 'linum-mode)
 
-(defadvice forward-paragraph (after forward-paragraph-linum-update)
-  "Perform (linum-update-current) after jumping forward one
-  paragraph to ensure line numbers are being rendered
-  correctly."
-  (linum-update-current))
-(ad-activate 'forward-paragraph)
+(unless (version< emacs-version "24")
+  (defadvice forward-paragraph (after forward-paragraph-linum-update)
+    "Perform (linum-update-current) after jumping forward one
+     paragraph to ensure line numbers are being rendered
+     correctly."
+    (linum-update-current))
+  (ad-activate 'forward-paragraph))
 
 (provide 'linenum)
