cd526d06218d63fb4928af37fff70e9bea65eb3f
Eric Nilsson <caterhatterpillar@gmail.com>
2020-08-13 08:54:37 +0200

    Refactor

--- init.el
+++ init.el
@@ -16,40 +16,43 @@
 
 (setq load-path (cons "~/.emacs.d/elisp" load-path))
 
+;; Aliases, bindings, and functions are better organized in separate files
 (require 'aliases)
-(require 'appearance)
 (require 'keybind)
-(require 'linenum)
 (require 'miscfun)
+
 (require 'numbers)
-(require 'programming)
 (require 'template)
-(require 'custom-python)
 
-(require 'midnight)  ; Clean up buffers after a while
+;;; APPEARANCE
 
-(add-to-list 'load-path "/usr/share/stgit/contrib")  ; TODO
-(require 'stgit)
+(let ((theme-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+      (load-custom-theme (lambda ()
+                           (add-to-list 'custom-theme-load-path theme-path)
+                           (load-theme 'solarized t)
+                           (set-frame-parameter nil 'background-mode 'dark)
+                           (enable-theme 'solarized))))
+  (unless (or (version< emacs-version "24") (not (display-graphic-p)))
+      (funcall load-custom-theme)))
 
-;; Prevent trailing whitespace in modified lines
-(when (eq system-type 'gnu/linux)  ; no diff on windows
-  (require 'diff)
-  (defun delete-trailing-whitespace-at-modified-lines ()
-    (when (and buffer-file-name (derived-mode-p 'prog-mode))
-      (save-excursion
-        (with-current-buffer
-            (diff-no-select buffer-file-name (current-buffer) nil 'noasync)
-          (diff-delete-trailing-whitespace)
-          (kill-buffer)))))
+(setq frame-title-format  ; Full filename in titlebar
+      '((:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name)) "%b"))))
 
-  (add-hook 'before-save-hook 'delete-trailing-whitespace-at-modified-lines))
+(when (display-graphic-p)
+  (menu-bar-mode -1)
+  (toggle-scroll-bar -1)
+  (tool-bar-mode -1))
 
+(setq inhibit-startup-message t)
 
-;; Set these so that you can call conda in the compilation environment
-(setq shell-file-name "bash")
-(setq shell-command-switch "-ic")
+(setq column-number-mode t)
 
-;; State
+(blink-cursor-mode -1)
+(mouse-avoidance-mode 'jump)
+(setq x-stretch-cursor t)
+
+;;; HISTORY & BACKUP
 
 (setq savehist-file "~/.emacs.d/history")
 (setq savehist-additional-variables
@@ -64,7 +67,9 @@
       kept-old-versions 2
       version-control t)
 
-;; Buffers
+;;; BUFFERS
+
+(require 'midnight)  ; Clean up buffers after a while
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
@@ -79,12 +84,98 @@
 (setq scroll-step            1
       scroll-conservatively  10000)
 
-;; Windows
+(add-hook 'prog-mode-hook 'linum-mode)
+
+(unless (version< emacs-version "24")
+  (defadvice forward-paragraph (after forward-paragraph-linum-update)
+    "Perform (linum-update-current) after jumping forward one
+     paragraph to ensure line numbers are being rendered
+     correctly."
+    (linum-update-current))
+  (ad-activate 'forward-paragraph))
+
+;; Prevent trailing whitespace in modified lines
+(when (eq system-type 'gnu/linux)  ; no diff on windows
+  (require 'diff)
+  (defun delete-trailing-whitespace-at-modified-lines ()
+    (when (and buffer-file-name (derived-mode-p 'prog-mode))
+      (save-excursion
+        (with-current-buffer
+            (diff-no-select buffer-file-name (current-buffer) nil 'noasync)
+          (diff-delete-trailing-whitespace)
+          (kill-buffer)))))
+
+  (add-hook 'before-save-hook 'delete-trailing-whitespace-at-modified-lines))
+
+;;; PROGRAMMING
+
+(require 'whitespace)
+(setq whitespace-style '(empty face lines-tail tabs trailing))
+(global-whitespace-mode t)
+
+(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                  global-semanticdb-minor-mode
+                                  global-semantic-decoration-mode
+                                  global-semantic-highlight-func-mode
+                                  global-semantic-stickyfunc-mode
+                                  global-semantic-idle-summary-mode
+                                  global-semantic-idle-breadcrumbs-mode
+                                  global-semantic-mru-bookmark-mode))
+
+(add-hook 'prog-mode-hook 'semantic-mode)
+
+(add-hook 'prog-mode-hook 'show-paren-mode)
+
+(setq-default indent-tabs-mode nil)
+(setq initial-scratch-message nil)
+
+(setq grep-scroll-output t)
+
+(add-hook 'c-mode-hook
+          '(lambda () (define-key c-mode-map "\C-m" 'newline-and-indent)))
+
+(when (display-graphic-p)
+  (add-to-list 'compilation-finish-functions 'notify-compilation-complete))
+
+(defun notify-compilation-complete(buffer msg)
+  (if (string-match "^finished" msg)
+      (tooltip-show "\n Compilation Success \n ")
+    (tooltip-show "\n Compilation Failure \n ")))
+
+(setq compilation-scroll-output 'first-error)
+
+;; Load stgit after loading a custom theme
+(add-to-list 'load-path "/usr/share/stgit/contrib")  ; TODO
+(require 'stgit)
+
+;;; PYTHON
+
+(elpy-enable)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+(add-hook 'python-mode-hook
+          '(lambda () (highlight-indentation-mode 0)))  ; Looks terrible
+
+(remove-hook 'elpy-modules 'elpy-module-highlight-indentation)
+
+(pyvenv-activate "~/anaconda3/envs/tensorflow")  ; TODO
+
+;; Set these to enable conda in the compilation environment
+(setq shell-file-name "bash")
+(setq shell-command-switch "-ic")
+
+;;; WINDOWS
 
 (winner-mode 1)
 (windmove-default-keybindings)
 
-;; Misc
+;;; MISC
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq font-lock-global-modes '(not test-log-mode))
@@ -98,10 +189,8 @@
            (add-hook hook (lambda () (flyspell-mode -1))))
          (add-hook 'prog-mode-hook (lambda () (flyspell-prog-mode)))))
 
-;; Candidates
-;; ----------
+;;; CANDIDATES
 
-;; If on your MAC, do this:
 (setq mac-option-modifier nil
       mac-command-modifier 'meta
       x-select-enable-clipboard t)
@@ -109,6 +198,7 @@
 ;; Prefer vertical splits on my home system
 (setq split-height-threshold nil)
 (setq split-width-threshold 160)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
