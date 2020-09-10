abbebf262c56e244233655dbaea9029c16d8bca7
Eric Nilsson <eric.nilsson2@saabgroup.com>
2020-09-10 16:22:48 +0200

    Add prettier jsx formatting

--- init.el
+++ init.el
@@ -210,6 +210,9 @@
 (require 'indium)
 (add-hook 'js-mode-hook #'indium-interaction-mode)
 
+(require 'prettier-js)
+(add-hook 'rjsx-mode-hook 'prettier-js-mode)
+
 ;;; WINDOWS
 
 (winner-mode 1)
@@ -265,7 +268,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yafolding indium conda flycheck elpy anaconda-mode))))
+    (prettier-js prettier yafolding indium conda flycheck elpy anaconda-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
