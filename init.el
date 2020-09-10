94074e1575890512ade02766ea5d690782c2fdbc
Eric Nilsson <eric.nilsson2@saabgroup.com>
2020-09-10 16:26:10 +0200

    I don't use semantic mode anymore

--- init.el
+++ init.el
@@ -130,17 +130,6 @@
 (setq whitespace-style '(empty face lines-tail tabs trailing))
 (global-whitespace-mode t)
 
-(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
-                                  global-semanticdb-minor-mode
-                                  global-semantic-decoration-mode
-                                  global-semantic-highlight-func-mode
-                                  global-semantic-stickyfunc-mode
-                                  global-semantic-idle-summary-mode
-                                  global-semantic-idle-breadcrumbs-mode
-                                  global-semantic-mru-bookmark-mode))
-
-(add-hook 'prog-mode-hook 'semantic-mode)
-
 (add-hook 'prog-mode-hook 'show-paren-mode)
 
 (setq-default indent-tabs-mode nil)
