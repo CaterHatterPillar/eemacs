8501e1ec38649d96a9c81267e554702db9a6c090
Eric Nilsson <eric.nilsson2@saabgroup.com>
2022-05-04 13:14:52 +0200

    Prevent trailing whitespace in modified lines

--- init.el
+++ init.el
@@ -67,3 +67,15 @@
 (load custom-file 'noerror)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Prevent trailing whitespace in modified lines
+(when (eq system-type 'gnu/linux)  ; no diff on windows
+  (require 'diff)
+  (defun delete-trailing-whitespace-at-modified-lines ()
+    (when (and buffer-file-name (derived-mode-p 'prog-mode))
+      (save-excursion
+	(with-current-buffer
+	    (diff-no-select buffer-file-name (current-buffer) nil 'noasync)
+	  (diff-delete-trailing-whitespace)
+	  (kill-buffer)))))
+  (add-hook 'before-save-hook 'delete-trailing-whitespace-at-modified-lines))
