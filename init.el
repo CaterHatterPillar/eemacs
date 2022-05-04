CaterHatterPillar/eemacs

commit f3068b7203b89fe65e73e8fab528b72d3672366b

Configure pyvenv

init.el
@@ -36,3 +36,10 @@
 (setq x-stretch-cursor t)
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(use-package pyvenv
+  :ensure t
+  :init
+  (setenv "WORKON_HOME" "~/.pyenv/versions")
+  (setq pyvenv-workon "emacs")  ; fallback
+  (pyvenv-tracking-mode 1))  ; use add-dir-local-variable to set 'pyenv-workon'

commit fd8ee5524f0e186d48c39b186354d324068c0edb

Correct regression introduced in f3068b7

init.el
@@ -21,7 +21,7 @@
 
 (use-package pyvenv
   :ensure t
-  :init
+  :config
   (setenv "WORKON_HOME" "~/.pyenv/versions")
   (setq pyvenv-workon "emacs")  ; fallback
   (pyvenv-tracking-mode 1))  ; use add-dir-local-variable to set 'pyenv-workon'