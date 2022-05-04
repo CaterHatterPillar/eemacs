144e0a48a524a6a37fc8a65bbbffc30cb3089dfd
Eric Nilsson <eric.nilsson2@saabgroup.com>
2022-05-04 09:42:56 +0200

    Configure package

---
+++ init.el
@@ -0,0 +1,6 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
