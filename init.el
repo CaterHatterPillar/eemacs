f6b60f8118c931e602d51e1a1ee8f755f18690a6
Eric Nilsson <eric.nilsson2@saabgroup.com>
2022-05-05 14:49:20 +0200

    Configure backups

--- init.el
+++ init.el
@@ -111,3 +111,17 @@
 	  (diff-delete-trailing-whitespace)
 	  (kill-buffer)))))
   (add-hook 'before-save-hook 'delete-trailing-whitespace-at-modified-lines))
+
+(defvar backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p backup-directory))
+    (make-directory backup-directory t))
+
+(setq backup-directory-alist `(("." . ,backup-directory)))
+(setq make-backup-files t
+      backup-by-copying t
+      version-control t
+      delete-old-versions t
+      delete-by-moving-to-trash t
+      kept-old-versions 6
+      kept-new-versions 9
+      auto-save-default t)
