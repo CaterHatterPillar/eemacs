902e5594bf7fe4271e3785ad82f592e4349a36fb
Eric Nilsson <caterhatterpillar@gmail.com>
2022-08-29 15:55:30 +0200

    Inherit TODO state from subtasks

--- init.el
+++ init.el
@@ -152,3 +152,10 @@
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 (global-set-key (kbd "C-.") 'next-error)
 (global-set-key (kbd "C-,") 'previous-error)
+
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook #'org-summary-todo)
