f2b5fdfbad7a0ac16e3c8029d6e578b8ae805056
Eric Nilsson <eric@gronska.org>
2023-01-20 14:12:19 +0100

    Remove counsel and ivy

--- init.el
+++ init.el
@@ -121,25 +121,6 @@
   (setq beacon-blink-when-point-moves nil)
   (beacon-mode 1))
 
-(use-package counsel
-  :ensure t)
-
-(use-package ivy
-  :ensure t
-  :config
-  (ivy-mode)
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t)
-  (global-set-key "\C-s" 'swiper)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c k") 'counsel-ag)
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
-
 (use-package rg
   :ensure t)
 
