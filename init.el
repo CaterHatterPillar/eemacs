c12bcb9b9beb5282e076438aba6b7b934c1c8e0a
Eric Nilsson <eric@gronska.org>
2023-01-20 14:12:03 +0100

    Configure company and flycheck

--- init.el
+++ init.el
@@ -63,6 +63,26 @@
                          (require 'lsp-pyright)
                          (lsp))))  ; or lsp-deferred
 
+(use-package company
+  :ensure t
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-selection))
+  ;;       (:map lsp-mode-map
+  ;;        ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :ensure t
+  :hook (company-mode . company-box-mode))
+
+(use-package flycheck
+  :config
+  (global-flycheck-mode))
+
 (use-package python-black
   :ensure t
   :after python
